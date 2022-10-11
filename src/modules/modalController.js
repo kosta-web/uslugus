713901dd0e69be39779ff09a8262ff114771a9dc
podyankee <podyankee@gmail.com>
2022-10-11 03:26:51 +0300

    day-7 finished

--- src/modules/modalController.js
+++ src/modules/modalController.js
@@ -5,6 +5,7 @@ export const modalController = ({
 	parentBtns,
 	time = 300,
 	handlerOpenModal = () => {},
+	handlerCloseModal = () => {},
 }) => {
 	const handlerElems = parentBtns
 		? document.querySelector(parentBtns)
@@ -18,47 +19,56 @@ export const modalController = ({
     transition: opacity ${time}ms ease-in-out;
   `;
 
-	const event = {
+	const data = {
 		handlerOpenModal,
+		handlerCloseModal,
 		onOpenModal(handlerOpenModal) {
-			event.handlerOpenModal = handlerOpenModal;
+			data.handlerOpenModal = handlerOpenModal;
 		},
-	};
+		onCloseModal(handlerCloseModal) {
+			data.handlerCloseModal = handlerCloseModal;
+		},
+		closeModal: event => {
+			const target = event.target;
 
-	const closeModal = event => {
-		const target = event.target;
+			if (
+				target === modalElem ||
+				(btnClose && target.closest(btnClose)) ||
+				event.code === 'Escape' ||
+				event.type === 'submit'
+			) {
+				modalElem.style.opacity = '0';
 
-		if (target === modalElem || (btnClose && target.closest(btnClose)) || event.code === 'Escape') {
-			modalElem.style.opacity = 0;
+				setTimeout(() => {
+					modalElem.style.visibility = 'hidden';
+					data.handlerCloseModal(modalElem);
+				}, time);
 
-			setTimeout(() => {
-				modalElem.style.visibility = 'hidden';
-			}, time);
-
-			window.removeEventListener('keydown', closeModal);
-		}
-	};
-
-	const openModal = async () => {
-		await event.handlerOpenModal();
-		modalElem.style.visibility = 'visible';
-		modalElem.style.opacity = 1;
-		window.addEventListener('keydown', closeModal);
+				window.removeEventListener('keydown', data.closeModal);
+			}
+		},
+		openModal: async (handler) => {
+			await data.handlerOpenModal({handler, modalElem});
+			modalElem.style.visibility = 'visible';
+			modalElem.style.opacity = '1';
+			window.addEventListener('keydown', data.closeModal);
+		},
 	};
 
 	if (parentBtns) {
 		handlerElems.addEventListener('click', ({ target }) => {
-			if (target.closest(btnOpen)) {
-				openModal();
+			const handler = target.closest(btnOpen);
+			if (handler) {
+				data.openModal(handler);
 			}
 		});
 	} else {
 		handlerElems.forEach(btn => {
-			btn.addEventListener('click', openModal);
+			btn.addEventListener('click', data.openModal);
 		});
 	}
 
-	modalElem.addEventListener('click', closeModal);
+	modalElem.addEventListener('click', data.closeModal);
 
-	return event;
+	return data;
 };
---
+++ src/modules/sign.js
@@ -0,0 +1,61 @@
+import { avatarController } from './avatarController';
+import { postData } from './postData';
+import { API_URL } from './const';
+import { createCard } from './createCard';
+import { auth } from './auth';
+
+export const signInController = callback => {
+	const form = document.querySelector('.form-sign-in');
+
+	form.addEventListener('submit', async e => {
+		e.preventDefault();
+
+		const formData = new FormData(form);
+		const data = Object.fromEntries(formData);
+		
+		const dataResponse = await postData(`${API_URL}/api/service/signin`, data);
+
+		if (dataResponse.errors) {
+			console.log(dataResponse.errors); // todo обработка ошибок
+			return;
+		}
+		callback(e);
+		auth(dataResponse);
+	});
+};
+
+export const signUpController = callback => {
+	const form = document.querySelector('.form-sign-up');
+
+	const crp = avatarController({
+		inputFile: '.avatar__input',
+		uploadResult: '.avatar__result',
+	});
+
+	form.addEventListener('submit', async e => {
+		e.preventDefault();
+		if (form.password[0].value !== form.password[1].value) {
+			console.log('Пароли не совпадают'); // todo обработка ошибки
+			return;
+		}
+		const formData = new FormData(form);
+		const data = Object.fromEntries(formData);
+		data.avatar = await crp.result({
+			type: 'base64',
+			size: 'viewport',
+		});
+		const dataResponse = await postData(`${API_URL}/api/service/signup`, data);
+
+		if (dataResponse.errors) {
+			console.log(dataResponse.errors); // todo обработка ошибок
+			return;
+		}
+		const servicesList = document.querySelector('.services__list');
+		servicesList.append(createCard(dataResponse));
+
+		auth(dataResponse);
+		form.reset();
+		crp.hideAvatar();
+		callback(e);
+	});
+};
